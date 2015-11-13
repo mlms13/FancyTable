366d2aaa3a6e8ecf0db597fe6c7fae8fb6553c49
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-11-13 09:27:48 -0700

    More WIP for column fixing

--- src/fancy/table/GridContainer.hx
+++ src/fancy/table/GridContainer.hx
@@ -7,7 +7,7 @@ class GridContainer {
   // public var topLeft(default, null) : Element;
   public var top(default, null) : Element;
   // public var topRight(default, null) : Element;
-  // public var left(default, null) : Element;
+  public var left(default, null) : Element;
   // public var content(default, null) : Element;
   // public var right(default, null) : Element;
   // public var bottomLeft(default, null) : Element;
@@ -16,10 +16,13 @@ class GridContainer {
   public var grid(default, null) : Element;
 
   public function new() {
-    grid = Dom.create("div.ft-table-grid-contaienr");
     top = Dom.create("div.ft-table-fixed-top");
+    left = Dom.create("div.ft-table-fixed-left");
+    grid = Dom.create("div.ft-table-grid-contaienr");
 
-    grid.appendChild(top);
+    grid
+      .prependChild(left)
+      .prependChild(top);
   }
 
   public function positionPanes(deltaTop : Int, deltaLeft : Int) {
--- src/fancy/table/Row.hx
+++ src/fancy/table/Row.hx
@@ -8,10 +8,10 @@ using thx.Objects;
 
 class Row {
   public var el(default, null) : Element;
+  public var rows(default, null) : Array<Row>;
+  public var cols(default, null) : Array<Column>;
   var cellsEl : Element;
   var opts : FancyRowOptions;
-  var cols : Array<Column>;
-  var rows : Array<Row>;
 
   public function new(?cols : Array<Column>, ?colCount = 0, ?options : FancyRowOptions) {
     this.cols = cols == null ? [] : cols;
