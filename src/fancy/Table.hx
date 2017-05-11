4f6b889f2fd0d2fe2d7129787779d3c1e0a1e968
Andy White <greatwhitehope@gmail.com>
2017-05-11 09:08:21 -0600

    Add scroll functions

--- src/fancy/Table.hx
+++ src/fancy/Table.hx
@@ -422,6 +422,14 @@ class Table {
     return grid.getScrollPosition();
   }
 
+  public function scrollToPosition(?x : Float, ?y : Float) : Void {
+    grid.scrollToPosition(x, y);
+  }
+
+  public function scrollTo(?xPos: HorizontalScrollPosition, ?yPos: VerticalScrollPosition) : Void {
+    grid.scrollTo(xPos, yPos);
+  }
+
   public function resetScroll() : Void {
     grid.scrollTo(settings.initialScrollX, settings.initialScrollY);
   }
